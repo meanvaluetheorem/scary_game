a4eb986f1a2edcbede0a2a112ba0781aab5583a1
32gurihs@gmail.com <32gurihs@gmail.com>
2021-05-25 01:36:25 +0900

    이미지 추가 앞,오른쪽,왼쪽

--- scary_game.cpp
+++ scary_game.cpp
@@ -4,7 +4,7 @@
 #include<math.h>
 int x, y;
 SceneID sc;
-ObjectID obj, sb, rb, rbb, eeb, eb, startsc, gamesc1, scary;
+ObjectID obj, sb, rb, rbb, eeb, eb, startsc, gamesc_front, gamesc_right, gamesc_left, scary;
 ObjectID Object(const char* image, SceneID scene, int x, int y, bool shown) {
 	ObjectID object = createObject(image);
 	locateObject(object, scene, x, y);
@@ -29,7 +29,9 @@ int main() {
 	setMouseCallback(mouseCallback);
 	sc = createScene("", "\\images\\sc.png");
 	//startsc = Object("\\images\\startsc.png", sc, 0, 0, true);
-	gamesc1 = Object("\\images\\gamesc1.png", sc, 0, 0, true);
+	gamesc_front = Object("\\images\\gamesc_front.png", sc, 0, 0, true);
+	gamesc_right = Object("\\images\\gamesc_right.png", sc, 0, 0, false);
+	gamesc_left = Object("\\images\\gamesc_left.png", sc, 0, 0, false);
 	sb = Object("\\images\\start.png", sc, 500, 45, true);
 	rb = Object("\\images\\restart.png", sc, 500, 45, false);
 	//eeb = Object("\\images\\end.png", sc, 500, 10, true);
