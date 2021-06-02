d4276ecd67058831a3fe00ceb036133243cf58c1
32gurihs@gmail.com <32gurihs@gmail.com>
2021-06-02 23:28:57 +0900

    14.6.5 endingsceneadded

--- scary_game.cpp
+++ scary_game.cpp
@@ -1,15 +1,13 @@
 ﻿///////////////////////////////////////////////   띄어쓰기 줄바꾸기 금지   ///////////////////////////////////////////////
 #include<stdio.h>
 #include<bangtal.h>
-#include<windows.h>
-#include<math.h>
 int hit = 0, key_count = -1, scene_add = 0, key_input[4] = { 0, };
 SoundID scary_BGM;
 TimerID timechong;
-SceneID sc_start, sc_front, sc_back, sc_right, sc_left, sc_up, sc_down, sc_roof, sc_scary, sc_keypan1, sc_keypan2;
+SceneID sc_start, sc_front, sc_back, sc_right, sc_left, sc_up, sc_down, sc_roof, sc_scary, sc_keypan1, sc_keypan2, sc_win;
 ObjectID sb, pb, eb, start_sc, light_1, light_2, key, chong, sonchong, chongtang, glasses, glasses_wire, keypan1, keypan2;
 ObjectID wire_item, wire, blood, empty_blood, hammer, huge, win, look_right, look_left, look_up, look_down;
-ObjectID gamesc_front, gamesc_right, gamesc_left, gamesc_up, gamesc_down, gamesc_back, gamesc_roof, numpan1, numpan2;
+ObjectID ending, gamesc_front, gamesc_right, gamesc_left, gamesc_up, gamesc_down, gamesc_back, gamesc_roof, numpan1, numpan2;
 ObjectID roofin, roofin1, roofin2, roofin3, roofinf, roofout, scary, small_scary, no_scary, empty_scary, num[4];
 ObjectID Object(const char* image, SceneID scene, int x, int y, bool shown) {
 	ObjectID object = createObject(image);
@@ -21,11 +19,6 @@ SoundID playsound(SoundID sound, const char* soundname, const char* soundfile) {
 	sound = createSound(soundfile);
 	playSound(sound, true);
 	return sound;
-}
-void bang() {
-	hideObject(sonchong);
-	showObject(chongtang);
-	startTimer(timechong);
 }
 void goscene(SceneID scene) {
 	enterScene(scene);
@@ -51,17 +44,13 @@ void goscene(SceneID scene) {
 	else if (scene == sc_roof) scene_add = 9;
 	else if (scene == sc_keypan2) { scene_add = 10; locateObject(look_down, scene, 215, 256); }
 }
-void winwin() {
-	stopSound(scary_BGM);
-	showMessage("DORE OPENED\n");
-	showObject(win);
-	goscene(sc_front);
-}
+void bang() {hideObject(sonchong);showObject(chongtang);startTimer(timechong);}
+void winwin() {stopSound(scary_BGM);enterScene(sc_win);goscene(sc_win);}
 void timerComtrol(TimerID timer) {
 	if (timer == timechong) {
 	hideObject(chongtang);
 	showObject(sonchong);
-	timechong = createTimer(0.1);
+	setTimer(timechong, 0.1f);
 	}
 }
 void keyboardControl(KeyCode code, KeyState state) {
@@ -113,7 +102,8 @@ void mouseControl(ObjectID obj, int x, int y, MouseAction act) {
 	if (obj == sb) goscene(sc_front);
 	else if (obj == pb) goscene(sc_start);
 	else if (obj == eb)endGame();
-	else if (obj == win)endGame();
+	else if (obj == win)showObject(ending);
+	else if (obj == ending)endGame();
 	else if (obj == wire_item) pickObject(wire_item);
 	else if (obj == glasses) pickObject(glasses);
 	else if (obj == glasses_wire) pickObject(glasses_wire);
@@ -149,65 +139,65 @@ int main() {
 	setKeyboardCallback(keyboardControl);
 	setMouseCallback(mouseControl);
 	setTimerCallback(timerComtrol);
-	{
-		timechong = createTimer(0.1);
-		sc_start = createScene("", "\\images\\sc.png");
-		sc_front = createScene("", "\\images\\sc.png");
-		sc_back = createScene("", "\\images\\sc.png");
-		sc_right = createScene("", "\\images\\sc.png");
-		sc_left = createScene("", "\\images\\sc.png");
-		sc_up = createScene("", "\\images\\sc.png");
-		sc_down = createScene("", "\\images\\sc.png");
-		sc_scary = createScene("", "\\images\\sc.png");
-		sc_roof = createScene("", "\\images\\sc.png");
-		sc_keypan1 = createScene("", "\\images\\sc.png");
-		sc_keypan2 = createScene("", "\\images\\sc.png");
-		start_sc = Object("\\images\\start_sc.png", sc_start, 0, 0, true);
-		sb = Object("\\images\\start.png", sc_start, 38, 55, true);
-		eb = Object("\\images\\end.png", sc_start, 368, 55, true);
-		pb = Object("\\images\\pause.png", sc_front, 500, 55, true);
-		gamesc_front = Object("\\images\\gamesc_front.png", sc_front, 0, 0, true);
-		numpan2 = Object("\\images\\numpan2.png", sc_front, 420, 316, true);
-		no_scary = Object("\\images\\no_scary.png", sc_front, 304, 396, true);
-		small_scary = Object("\\images\\small_scary.png", sc_front, 304, 396, true);
-		empty_scary = Object("\\images\\empty_scary.png", sc_scary, 0, 0, true);
-		hammer = Object("\\images\\hammer.png", sc_scary, 0, 0, false);
-		huge = Object("\\images\\huge.png", sc_scary, 370, 0, false);
-		scary = Object("\\images\\scary.png", sc_scary, 0, 0, true);
-		gamesc_right = Object("\\images\\gamesc_right.png", sc_right, 0, 0, true);
-		gamesc_left = Object("\\images\\gamesc_left.png", sc_left, 0, 0, true);
-		numpan1 = Object("\\images\\numpan1.png", sc_left, 31, 305, true);
-		gamesc_back = Object("\\images\\gamesc_back.png", sc_back, 0, 0, true);
-		blood = Object("\\images\\blood.png", sc_back, 31, 283, true);
-		empty_blood = Object("\\images\\empty_blood.png", sc_back, 0, 0, true);
-		gamesc_up = Object("\\images\\gamesc_up.png", sc_up, 0, 0, true);
-		roofinf = Object("\\images\\roofinf.png", sc_up, 151, 181, true);
-		roofin3 = Object("\\images\\roofin3.png", sc_up, 151, 181, true);
-		roofin2 = Object("\\images\\roofin2.png", sc_up, 151, 181, true);
-		roofin1 = Object("\\images\\roofin1.png", sc_up, 151, 181, true);
-		roofin = Object("\\images\\roofin.png", sc_up, 151, 181, true);
-		key = Object("\\images\\key.png", sc_up, 438, 144, true);
-		light_1 = Object("\\images\\light.png", sc_up, 13, 122, true);
-		light_2 = Object("\\images\\light.png", sc_up, 420, 121, true);
-		gamesc_down = Object("\\images\\gamesc_down.png", sc_down, 0, 0, true);
-		chong = Object("\\images\\chong.png", sc_down, 131, 229, true);
-		sonchong = Object("\\images\\sonchong.png", sc_down, 300, 0, false);/////////////////////
-		chongtang = Object("\\images\\chongtang.png", sc_down, 300, 0, false);///////////////////
-		gamesc_roof = Object("\\images\\gamesc_roof.png", sc_roof, 0, 0, true);
-		roofout = Object("\\images\\roofout.png", sc_roof, 175, 60, true);
-		glasses = Object("\\images\\glasses.png", sc_roof, 415, 163, true);
-		wire_item = Object("\\images\\wire_item.png", sc_roof, 171, 280, true);
-		glasses_wire = Object("\\images\\glasses_wire.png", sc_roof, 422, 580, false);
-		keypan1 = Object("\\images\\keypan.png", sc_keypan1, 0, 0, true);
-		keypan2 = Object("\\images\\keypan.png", sc_keypan2, 0, 0, true);
-		look_right = Object("\\images\\look_right.png", sc_front, 470, 335, true);
-		look_left = Object("\\images\\look_left.png", sc_front, 0, 335, true);
-		look_up = Object("\\images\\look_up.png", sc_front, 215, 670, true);
-		look_down = Object("\\images\\look_down.png", sc_front, 215, 0, true);
-		wire = Object("\\images\\gamesc_wire.png", sc_front, 24, 0, false);
-		win = Object("\\images\\win.png", sc_front, 0, 0, false);
-		defineCombination(glasses, wire_item, glasses_wire);
-		scary_BGM = playsound(scary_BGM, "", "\\sounds\\BGM.mp3");
-	}
+	timechong = createTimer(0.1f);
+	sc_start = createScene("", "\\images\\sc.png");
+	sc_front = createScene("", "\\images\\sc.png");
+	sc_back = createScene("", "\\images\\sc.png");
+	sc_right = createScene("", "\\images\\sc.png");
+	sc_left = createScene("", "\\images\\sc.png");
+	sc_up = createScene("", "\\images\\sc.png");
+	sc_down = createScene("", "\\images\\sc.png");
+	sc_scary = createScene("", "\\images\\sc.png");
+	sc_roof = createScene("", "\\images\\sc.png");
+	sc_keypan1 = createScene("", "\\images\\sc.png");
+	sc_keypan2 = createScene("", "\\images\\sc.png");
+	sc_win = createScene("", "\\images\\sc.png");
+	start_sc = Object("\\images\\start_sc.png", sc_start, 0, 0, true);
+	sb = Object("\\images\\start.png", sc_start, 38, 55, true);
+	eb = Object("\\images\\end.png", sc_start, 368, 55, true);
+	pb = Object("\\images\\pause.png", sc_front, 500, 55, true);
+	gamesc_front = Object("\\images\\gamesc_front.png", sc_front, 0, 0, true);
+	numpan2 = Object("\\images\\numpan2.png", sc_front, 420, 316, true);
+	no_scary = Object("\\images\\no_scary.png", sc_front, 304, 396, true);
+	small_scary = Object("\\images\\small_scary.png", sc_front, 304, 396, true);
+	empty_scary = Object("\\images\\empty_scary.png", sc_scary, 0, 0, true);
+	hammer = Object("\\images\\hammer.png", sc_scary, 0, 0, false);
+	huge = Object("\\images\\huge.png", sc_scary, 370, 0, false);
+	scary = Object("\\images\\scary.png", sc_scary, 0, 0, true);
+	gamesc_right = Object("\\images\\gamesc_right.png", sc_right, 0, 0, true);
+	gamesc_left = Object("\\images\\gamesc_left.png", sc_left, 0, 0, true);
+	numpan1 = Object("\\images\\numpan1.png", sc_left, 31, 305, true);
+	gamesc_back = Object("\\images\\gamesc_back.png", sc_back, 0, 0, true);
+	blood = Object("\\images\\blood.png", sc_back, 31, 283, true);
+	empty_blood = Object("\\images\\empty_blood.png", sc_back, 0, 0, true);
+	gamesc_up = Object("\\images\\gamesc_up.png", sc_up, 0, 0, true);
+	roofinf = Object("\\images\\roofinf.png", sc_up, 151, 181, true);
+	roofin3 = Object("\\images\\roofin3.png", sc_up, 151, 181, true);
+	roofin2 = Object("\\images\\roofin2.png", sc_up, 151, 181, true);
+	roofin1 = Object("\\images\\roofin1.png", sc_up, 151, 181, true);
+	roofin = Object("\\images\\roofin.png", sc_up, 151, 181, true);
+	key = Object("\\images\\key.png", sc_up, 438, 144, true);
+	light_1 = Object("\\images\\light.png", sc_up, 13, 122, true);
+	light_2 = Object("\\images\\light.png", sc_up, 420, 121, true);
+	gamesc_down = Object("\\images\\gamesc_down.png", sc_down, 0, 0, true);
+	chong = Object("\\images\\chong.png", sc_down, 131, 229, true);
+	sonchong = Object("\\images\\sonchong.png", sc_down, 300, 0, false);/////////////////////
+	chongtang = Object("\\images\\chongtang.png", sc_down, 300, 0, false);///////////////////
+	gamesc_roof = Object("\\images\\gamesc_roof.png", sc_roof, 0, 0, true);
+	roofout = Object("\\images\\roofout.png", sc_roof, 175, 60, true);
+	glasses = Object("\\images\\glasses.png", sc_roof, 415, 163, true);
+	wire_item = Object("\\images\\wire_item.png", sc_roof, 171, 280, true);
+	glasses_wire = Object("\\images\\glasses_wire.png", sc_roof, 422, 580, false);
+	keypan1 = Object("\\images\\keypan.png", sc_keypan1, 0, 0, true);
+	keypan2 = Object("\\images\\keypan.png", sc_keypan2, 0, 0, true);
+	look_right = Object("\\images\\look_right.png", sc_front, 470, 335, true);
+	look_left = Object("\\images\\look_left.png", sc_front, 0, 335, true);
+	look_up = Object("\\images\\look_up.png", sc_front, 215, 670, true);
+	look_down = Object("\\images\\look_down.png", sc_front, 215, 0, true);
+	wire = Object("\\images\\gamesc_wire.png", sc_front, 24, 0, false);
+	win = Object("\\images\\win.png", sc_win, 0, 0, true);
+	ending = Object("\\images\\ending.png", sc_win, 0, 0, false);
+	defineCombination(glasses, wire_item, glasses_wire);
+	scary_BGM = playsound(scary_BGM, "", "\\sounds\\BGM.mp3");
 	startGame(sc_start);
 }
